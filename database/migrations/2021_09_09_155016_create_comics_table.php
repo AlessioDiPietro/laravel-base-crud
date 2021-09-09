feffb87ba88009cc844c58b604a71fc8763e4da4
AlessioDPT <alessio.recos@gmail.com>
2021-09-09 17:56:45 +0200

    ADD- db comic / migrate db/ ADD-controller

---
+++ database/migrations/2021_09_09_155016_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->text('description');
+            $table->text('thumb');
+            $table->tinyinteger('price')->unsigned();
+            $table->string('type',15);
+            $table->string('series',15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
