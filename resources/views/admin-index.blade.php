130ef1157276293bdca5e9d761e7622d9cb5970c
AlessioDPT <alessio.recos@gmail.com>
2021-09-09 17:21:44 +0200

    ADD- layout + partial

---
+++ resources/views/admin-index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.template')
+@section('title', 'area admin')
+
+@section('main-content')
+<div class="table-responsive">
+    <table class="table align-middle">
+      <thead>
+        <tr>
+          ...
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          ...
+        </tr>
+        <tr class="align-bottom">
+          ...
+        </tr>
+        <tr>
+          <td>...</td>
+          <td>...</td>
+          <td class="align-top">This cell is aligned to the top.</td>
+          <td>...</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+@endsection
